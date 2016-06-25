7e004c554daf94e941ba87248fb85b0323a0be88
Scott Schmidt <sjschmidt44@gmail.com>
2016-06-25 16:24:07 -0700

    Updated with data loading to DB. Starter code complete.

--- scripts/loadDB.js
+++ scripts/loadDB.js
@@ -1,15 +1,14 @@
 (function(module) {
   var zips = {};
-  // City, State, Zip, Population, Location
 
   zips.createTable = function(callback) {
-    // TODO: Set up a DB table for articles.
     webDB.execute(
       'CREATE TABLE IF NOT EXISTS zips (' +
       'id SERIAL PRIMARY KEY, ' +
       'city VARCHAR(255) NOT NULL, ' +
       'state VARCHAR(255) NOT NULL, ' +
-      'location VARCHAR(255) NOT NULL,' +
+      'latitude INTEGER NOT NULL,' +
+      'longitude INTEGER NOT NULL,' +
       'population INTEGER NOT NULL,' +
       'zip INTEGER NOT NULL)',
       function(result) {
@@ -20,7 +19,6 @@
   };
 
   zips.truncateTable = function(callback) {
-    // Delete all records from given table.
     webDB.execute(
       'DELETE FROM zips;',
       callback
@@ -28,23 +26,29 @@
   };
 
   zips.loadDB = function() {
-    $.get('/data/zips.json')
-    .done(function(data) {
-      console.log(data);
-    })
-    .fail(function(err) {
-      console.error(err);
+    zips.createTable();
+
+    webDB.execute('SELECT * FROM zips', function(rows) {
+      if (rows.length) {
+        return;
+      } else {
+        $.get('/data/zips.json')
+        .done(function(data) {
+          data.forEach(function(ele) {
+            webDB.execute(
+              [
+                {
+                'sql': 'INSERT INTO zips(city, state, latitude, longitude, population, zip) VALUES(?, ?, ?, ?, ?, ?);',
+                'data': [ele.city, ele.state, ele.loc[1], ele.loc[0], ele.pop, ele.zip],
+                }
+              ],
+              function(results) {
+
+              })
+            })
+          })
+      }
     })
   }
   module.zips = zips;
 })(window)
-
-
-
-
-// webDB.execute(
-//   'INSERT INTO zips(city, state, location, population, zip) VALUES(?, ?, ?, ?, ?)'
-//   [],
-//   function(results) {
-//
-//   })
