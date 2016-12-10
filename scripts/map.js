3449877c4bf03f1b509c397e352627c1b9f3da4d
Scott Schmidt <sjschmidt44@gmail.com>
2016-12-10 09:40:13 -0800

    update code base to es6 syntax

--- scripts/map.js
+++ scripts/map.js
@@ -1,63 +1,62 @@
 (function(module) {
-  var map = {};
+  'use strict'
+
+  let map = {}
 
   function initMap(results) {
     if (results) {
-      var firstCity = results[0];
-      var loc = {lat: firstCity.latitude, lng: firstCity.longitude};
+      let firstCity = results[0]
+      let loc = {lat: firstCity.latitude, lng: firstCity.longitude}
       map.map = new google.maps.Map(document.getElementById('map'), {
         center: loc,
         scrollwheel: true,
         zoom: 8
-      });
+      })
 
-      results.forEach(function(city, idx) {
-        var marker = 'marker' + idx;
-        var infowindow = 'infowindow' + idx;
+      results.forEach((city, idx) => {
+        let marker = `marker${idx}`
+        let infowindow = `infowindow${idx}`
 
         map[infowindow] = new google.maps.InfoWindow({
-          content:
-          '<div>' +
-          '<h5>' + city.city + ', ' + city.state + '</h5>' +
-          '<p>Latitude: ' + city.latitude + '</p>' +
-          '<p>Longitude: ' + city.longitude + '</p>' +
-          '<p>Population: ' + city.population + '</p>' +
-          '</div>'
-        });
+          content: `
+          <div>
+          <h5>${city.city}, ${city.state}</h5>
+          <p>Latitude: ${city.latitude}</p>
+          <p>Longitude: ${city.longitude}</p>
+          <p>Population: ${city.population}</p>
+          </div>
+          `
+        })
 
         map[marker] = new google.maps.Marker({
           position: {lat: city.latitude, lng: city.longitude},
           map: map.map,
-          title: city.city + ', ' + city.state
+          title: `${city.city}, ${city.state}`
         })
 
-        map[marker].addListener('click', function() {
-          map[infowindow].open(map.map, map[marker]);
-        });
+        map[marker].addListener('click', () => map[infowindow].open(map.map, map[marker]))
       })
     } else {
       // This handles default location on load
-      var loc = {lat: 47.3623, lng: -122.1950};
+      let loc = {lat: 47.3623, lng: -122.1950}
       map.map = new google.maps.Map(document.getElementById('map'), {
         center: loc,
         scrollwheel: true,
         zoom: 8
-      });
+      })
 
       map.infowindow = new google.maps.InfoWindow({
         content: 'Seattle, WA'
-      });
+      })
 
       map.marker = new google.maps.Marker({
         position: loc,
         map: map.map,
         title: 'Hello World!'
-      });
+      })
 
-      map.marker.addListener('click', function() {
-        map.infowindow.open(map.map, map.marker);
-      });
+      map.marker.addListener('click', () => map.infowindow.open(map.map, map.marker))
     }
   }
-  module.initMap = initMap;
+  module.initMap = initMap
 })(window)
--- scripts/search.js
+++ scripts/search.js
@@ -1,37 +1,34 @@
+'use strict';
+
 (function(module) {
   (function() {
-    $('#city-select').attr('disabled', 'true');
-    webDB.execute('SELECT DISTINCT state FROM zips ORDER BY state', function(results) {
-      results.forEach(function(ele) {
-        var optEle = $('<option value=' + ele.state + '>' + ele.state + '</option>');
-        $('#state-select').append(optEle);
+    $('#city-select').attr('disabled', 'true')
+    webDB.execute('SELECT DISTINCT state FROM zips ORDER BY state',
+    results => {
+      results.forEach(ele => {
+        $('#state-select').append(`<option value="${ele.state}">${ele.state}</option>`)
       })
     })
   })()
 
-  $('#state-select').on('change', function(e) {
-    $('#city-option').siblings().remove();
-    webDB.execute('SELECT DISTINCT city FROM zips where state="' + $(this).val() + '" ORDER BY city', function(results) {
-      if (results) $('#city-select').removeAttr('disabled');
-      results.forEach(function(ele) {
-        var optEle = $('<option value=' + ele.city.replace(' ', '_') + '>' + ele.city + '</option>')
-        $('#city-select').append(optEle);
+  $('#state-select').on('change', function() {
+    $('#city-option').siblings().remove()
+    webDB.execute(`SELECT DISTINCT city FROM zips where state="${$(this).val()}" ORDER BY city`,
+    results => {
+      if (results) $('#city-select').removeAttr('disabled')
+      results.forEach(ele => {
+        $('#city-select').append(`<option value="${ele.city.replace(' ', '_')}">${ele.city}</option>`)
       })
     })
   })
-
-  $('#city-select').on('change', function(e) {
-    webDB.execute(
-      'SELECT * FROM zips WHERE state="' + $('#state-select').val() + '" AND city="' + $(this).val().replace('_', ' ') + '"',
-      function(results) {
-        initMap(results);
-    })
+  $('#city-select').on('change', function() {
+    webDB.execute(`SELECT * FROM zips WHERE state="${$('#state-select').val()}" AND city="${$(this).val().replace('_', ' ')}"`,
+    results => initMap(results))
   })
 
-  $('#zip-search').on('submit', function(e) {
-    e.preventDefault();
-    webDB.execute('SELECT * FROM zips WHERE zip=' + e.target.zip.value, function(results) {
-      initMap(results);
-    })
+  $('#zip-search').on('submit', (e) => {
+    e.preventDefault()
+    webDB.execute(`SELECT * FROM zips WHERE zip=${e.target.zip.value}`,
+    results => initMap(results))
   })
 })(window)
